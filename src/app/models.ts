4e2e9a5c8ed884c9e2ac435db603640b4d605e67
909meters <55340955+909meters@users.noreply.github.com>
2021-03-26 02:47:08 +0600

    Add files via upload

---
+++ src/app/models.ts
@@ -0,0 +1,13 @@
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Recipe {
+  id: number;
+  name: string;
+  description: string;
+  tag: string;
+  image: string;
+
+}
